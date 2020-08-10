64ce171e4ceda9de96663eedcb726b3313fa49e0
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-08-10 14:40:31 +0700

    config vue.js modal to nuxt.js
    config vue.js modal to nuxt.js

---
+++ components/ExampleModal.vue
@@ -0,0 +1,11 @@
+<template>
+  <modal name="example-modal" height="auto">This is an example.</modal>
+</template>
+
+<script>
+export default {
+  name: 'ExampleModal',
+}
+</script>
+
+<style></style>
---
+++ plugins/vue-js-modal.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Modal from '~/assets/vue-js-modal/ssr.nocss'
+
+import '~/assets/vue-js-modal/styles.css'
+
+Vue.use(Modal)
